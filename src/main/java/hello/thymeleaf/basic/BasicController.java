9f3f32d523355ff7a64b61786251b904f1ef9ef1
josoya <thdi4564@nate.com>
2023-05-15 15:32:48 +0900

    thymeleaf 문법 기능

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -2,10 +2,18 @@ package hello.thymeleaf.basic;
 
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/basic")
@@ -23,5 +31,84 @@ public class BasicController {
         return "basic/text-unescape";
     }
 
+    @GetMapping("/variable")
+    public String variable(Model model) {
 
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        ArrayList<User> list = new ArrayList<>();
+
+        list.add(userA);
+        list.add(userB);
+
+        HashMap<String, User> map = new HashMap<>();
+
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+
+        return "basic/basic-objects";
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    @GetMapping("literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!!");
+        return "basic/literal";
+    }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUser(model);
+        return "basic/each";
+    }
+
+
+    private void addUser(Model model) {
+        ArrayList<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
+    }
 }
+
---
+++ src/main/java/hello/thymeleaf/basic/HelloBean.java
@@ -0,0 +1,10 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Component;
+
+@Component("helloBean")
+public class HelloBean {
+    public String hello(String data) {
+        return "Hello " + data;
+    }
+}
