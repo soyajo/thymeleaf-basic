188065860e443a91da39f7e1e2be5df1afe4acdb
josoya <thdi4564@nate.com>
2023-05-16 17:25:24 +0900

    thyemleaf javascript 기능

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -119,6 +119,15 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+
+        model.addAttribute("user", new User("userA", 10));
+        addUser(model);
+
+        return "basic/javascript";
+    }
+
 
     private void addUser(Model model) {
         ArrayList<User> list = new ArrayList<>();
